a4e182d973a47a7e0004814cf9ebc5672776dc45
cwern40 <chris.wernli@hotmail.com>
2019-06-19 18:38:15 -0600

    I changed line 13 in the Movie.js file to link to the correct movie.

--- client/src/Movies/Movie.js
+++ client/src/Movies/Movie.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import axios from 'axios';
+import { Route } from 'react-router-dom';
 
 export default class Movie extends Component {
   constructor(props) {
@@ -10,8 +11,7 @@ export default class Movie extends Component {
   }
 
   componentDidMount() {
-    // change this line to grab the id passed on the URL
-    const id = 1;
+    let id = this.props.match.params.id;
     this.fetchMovie(id);
   }
 
